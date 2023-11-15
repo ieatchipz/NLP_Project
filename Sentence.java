a82108caf661b37b45f011a05801a73aa0e0bb0f
joodalfadhel <150938753+joodalfadhel@users.noreply.github.com>
2023-11-15 13:09:29 -0500

    updated sentence and driver files (part 1 completed)

--- Sentence.java
+++ Sentence.java
@@ -13,8 +13,31 @@ class Sentence {
         this.timestamp = timestamp;
     }
 
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
     public String toString() {
         return "{author:" + author + ", sentence:\"" + text + "\", timestamp:\"" + timestamp + "\"}";
     }
 }
-
