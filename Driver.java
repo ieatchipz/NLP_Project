655bc36a22bb3af7cb37a21fe989c531f4fbc571
jood <150938753+joodalfadhel@users.noreply.github.com>
2023-11-16 10:07:04 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -4,21 +4,18 @@ import java.io.IOException;
 
 public class Driver {
     public static void main(String[] args) {
-        String csvFile = "Covid-19 Twitter Dataset (Apr-Jun 2020).csv"; // Replace with your CSV file path
+        String csvFile = "Covid-19 Twitter Dataset (Apr-Jun 2020).csv"; 
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             String line;
             
             while ((line = br.readLine()) != null) {
-                // Assuming CSV format: favorite_count, retweet_count, created_at, hashtags, original_author, user_mentions, place, clean_tweet
                  String[] data = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
 
                 if (data.length >= 8) {
                     Sentence sentence = new Sentence(data[7], data[4], data[2]);
                     System.out.println(sentence.toString());
-                } //else {
-                    //System.out.println("Invalid data: " + line);
-                //}
+                } 
             }
         } catch (IOException e) {
             e.printStackTrace();
