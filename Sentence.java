26d4e87bb37cebedc93409b026463298c966932d
ieatchipz <150938654+ieatchipz@users.noreply.github.com>
2023-11-16 18:32:53 -0500

    Sentence.java Checkstyle Update (Passed)

--- Sentence.java
+++ Sentence.java
@@ -3,18 +3,41 @@ import java.io.FileReader;
 import java.io.IOException;
 
 class Sentence {
-    private String text;
-    private String author;
-    private String timestamp;
+	private String text;
+	private String author;
+	private String timestamp;
+	
+	public Sentence(String text, String author, String timestamp) {
+		this.text = text;
+		this.author = author;
+		this.timestamp = timestamp;
+	}
 
-    public Sentence(String text, String author, String timestamp) {
-        this.text = text;
-        this.author = author;
-        this.timestamp = timestamp;
-    }
+	public String getText() {
+		return text;
+	}
 
-    public String toString() {
-        return "{author:" + author + ", sentence:\"" + text + "\", timestamp:\"" + timestamp + "\"}";
-    }
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String toString() {
+		return "{author:" + author + ", sentence:\"" + text + "\", timestamp:\"" + timestamp + "\"}";
+	}
 }
-
