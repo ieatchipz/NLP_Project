7d6d43a6364c95b9ec0af30d496cdfed5ce260c9
jood <150938753+joodalfadhel@users.noreply.github.com>
2023-12-03 23:33:06 -0500

    updated part 2 code (ed)

--- Driver.java
+++ Driver.java
@@ -1,27 +1,51 @@
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Collections;
 
-public class Driver {
-    public static void main(String[] args) {
-        String csvFile = "Covid-19 Twitter Dataset (Apr-Jun 2020).csv"; // Replace with your CSV file path
+public class Driver{
 
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-            String line;
-            
-            while ((line = br.readLine()) != null) {
-                // Assuming CSV format: favorite_count, retweet_count, created_at, hashtags, original_author, user_mentions, place, clean_tweet
-                 String[] data = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+  private static void readTwitterData(ArrayList<Sentence> tweets){
+    int ctr = 0;
+    try{
+      //open the csv file for reading
+      File file = new File("covid_10K.csv");
+      BufferedReader reader = new BufferedReader(new FileReader(file));
+      String line = null;
 
-                if (data.length >= 8) {
-                    Sentence sentence = new Sentence(data[7], data[4], data[2]);
-                    System.out.println(sentence.toString());
-                } //else {
-                    //System.out.println("Invalid data: " + line);
-                //}
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+      //loop through each line in the csv
+
+      while ((line = reader.readLine()) != null){
+        String strippedLine = line.strip(); //cut off the newline from the line read in
+        Sentence sentence = Sentence.convertLine(strippedLine);
+        tweets.add(sentence);
+        ctr++;
+      }
+    }catch(Exception e){
+        e.printStackTrace();
+        System.out.println(ctr);
     }
+  }
+
+  
+  public static void main(String[] args){
+    ArrayList <Sentence> sentences = new ArrayList<Sentence>();
+
+    //solution for Twitter data
+    readTwitterData(sentences);
+
+
+    for(Sentence sentence : sentences) {
+      System.out.println(sentence); //print out results
+  }
+
+  
+
+  }
+
 }
--- Sentence.java
+++ Sentence.java
@@ -1,20 +1,112 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.Arrays;
+import java.util.ArrayList;
 
-class Sentence {
-    private String text;
-    private String author;
-    private String timestamp;
+public class Sentence{
+  private String text;
+  private String author;
+  private String timestamp;
 
-    public Sentence(String text, String author, String timestamp) {
-        this.text = text;
-        this.author = author;
-        this.timestamp = timestamp;
+  public Sentence(String text, String author, String timestamp){
+    this.text = text;
+    this.author = author;
+    this.timestamp = timestamp;
+  }
+
+  public void setText(String text){
+    this.text = text;
+  }
+
+  public String getText(){
+    return text;
+  }
+
+  public void setAuthor(String author){
+    this.author = author;
+  }
+
+  public String getAuthor(){
+    return author;
+  }
+
+  public void setTimestamp(String timestamp){
+    this.timestamp = timestamp;
+  }
+
+  public String getTimestamp(){
+    return timestamp;
+  }
+
+  public String toString(){
+    return "{author:" + author + ", sentence:\"" + text + "\", timestamp:\"" + timestamp + "\"}";
+  }
+
+    // for Twitter COVID dataset
+    public static Sentence convertLine(String line){
+      //System.out.println(line);
+      String[] pieces = new String[8];
+
+      String basket = "";
+      int ctr = 0;
+      boolean startQuote = false;
+      for(int i = 0; i < line.length(); i++){
+        if (line.charAt(i) == ',' && startQuote == false){
+          pieces[ctr] = basket;
+          basket = "";
+          ctr++;
+        } else if (line.charAt(i) == '"'){
+          startQuote = ! startQuote;
+        } else{
+          basket += line.charAt(i);
+        }
+      }
+      pieces[ctr] = basket;
+      //System.out.println(Arrays.toString(pieces));
+      String date = pieces[2];
+      String username = pieces[4];
+      String tweet = pieces[7];
+  
+      tweet = tweet.replaceAll("\"", ""); //removes double quotations from tweet
+      tweet = tweet.replaceAll("\\.", ""); //removes periods from tweet
+      tweet = tweet.replaceAll(",", ""); //removes commas from tweet
+      tweet = tweet.replaceAll("!", ""); //removes exclamations from tweet
+      // you could have included more/other punctuation here to remove as well
+  
+      //clean the date from "4/19/2020 0:00" to "April 19 2020"
+      String [] datePieces = date.split(" ");
+      String first = datePieces[0];
+      datePieces = first.split("/");
+      String month = datePieces[0];
+      if (month.equals("1"))
+        month = "January";
+      else if (month.equals("2"))
+        month = "February";
+      else if (month.equals("3"))
+        month = "March";
+      else if (month.equals("4"))
+        month = "April";
+      else if (month.equals("5"))
+        month = "May";
+      else if (month.equals("6"))
+        month = "June";
+      else if (month.equals("7"))
+        month = "July";
+      else if (month.equals("8"))
+        month = "August";
+      else if (month.equals("9"))
+        month = "September";
+      else if (month.equals("10"))
+        month = "October";
+      else if (month.equals("11"))
+        month = "November";
+      else if (month.equals("12"))
+        month = "December";
+
+      date = month + " " + datePieces[1] + " " +  datePieces[2];
+  
+      String author = username;
+      String text = tweet;
+      return new Sentence(text, author, date);
     }
 
-    public String toString() {
-        return "{author:" + author + ", sentence:\"" + text + "\", timestamp:\"" + timestamp + "\"}";
-    }
+
 }
-
