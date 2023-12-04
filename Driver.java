d203e8486eeda0f954000b2fe549a16e3d3696d6
jood <150938753+joodalfadhel@users.noreply.github.com>
2023-12-04 18:42:08 -0500

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -5,8 +5,12 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map; //place with imports
 import java.util.Collections; //place with imports
+import java.util.Comparator;
 
 
 public class Driver{
@@ -17,7 +21,7 @@ public class Driver{
     int ctr = 0;
     try{
       //open the csv file for reading
-      File file = new File("./covid_10K.csv");
+      File file = new File("covid_10K.csv");
       BufferedReader reader = new BufferedReader(new FileReader(file));
       String line = null;
 
@@ -35,62 +39,55 @@ public class Driver{
     }
 	}
 
-  public static HashMap<String, Integer> printTopWords(ArrayList<Sentence> sentences) { //needs to go outside of main, new
-    HashMap<String, Integer> wordCount = new HashMap<>();
-    for (Sentence sentence : sentences) {
-      ArrayList<String> words = sentence.splitSentence();
-
-      for (String word : words) {
-          wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+    public static HashMap<String, Integer> printTopWords(ArrayList<Sentence> sentences){
+    HashMap<String,Integer> wordCounts = new HashMap<String,Integer>();
+    for(int i = 0; i < sentences.size(); i++){
+      Sentence sentence = sentences.get(i);
+      ArrayList <String> words = sentence.splitSentence();
+      for(int j = 0; j < words.size(); j++) {
+        if(wordCounts.get(words.get(j)) != null){
+        Integer count = wordCounts.get(words.get(j));
+        count = count + 1;
+        wordCounts.put(words.get(j), count);
+       } else 
+        wordCounts.put(words.get(j), (Integer) 1);
+        
       }
-  } //end of new
+    } 
+    return wordCounts; 
+    }
 
-  return wordCount;
-
-  }
-
+  
   public static void main(String[] args){
     ArrayList <Sentence> sentences = new ArrayList<Sentence>();
 
-    //solution for Twitter data
+    
     readTwitterData(sentences);
 
+
+
 	
-    for (Sentence sentence : sentences) { //this loop prints out the elements inside of the "splitSentence method"
-        ArrayList<String> words = sentence.splitSentence();
-        System.out.println("Tweet = " + sentence.getText());
-    	for (String word : words) {
-            System.out.println("Word = " + word);
-      }
+    // System.out.println(sentences.toString());
+    HashMap<String, Integer> sjHash = printTopWords(sentences); //calling method inside main, named it "sjHash" for "salsa, jood" lol
+    // System.out.println(sjHash.toString());
+    Map.Entry<String, Integer> maxEntry = null;
+    for (Map.Entry<String, Integer> entry : sjHash.entrySet())
+        if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0)
+            maxEntry = entry;
+    int maxValueLen = maxEntry.getValue().toString().length();
+    ArrayList <String> results = new ArrayList<String>();
+    for (Map.Entry set : sjHash.entrySet()){
+        String value = set.getValue().toString();
+        while(value.length() < maxValueLen)
+            value = " " + value;
+        results.add(value + " of " + set.getKey());
     }
-  
+    Collections.sort(results);
+    Collections.reverse(results);
+    for (int i = 0; i < results.size() && i < 100; i++)
+        System.out.println(results.get(i));
 
-  HashMap<String, Integer> sjHash = printTopWords(sentences); //calling method inside main, named it "sjHash" for "salsa, jood" lol
 
-  Map.Entry<String, Integer> maxEntry = null; //start of copied code from CS1111 website
-  for (Map.Entry<String, Integer> entry : sjHash.entrySet()) {
-      if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
-          maxEntry = entry;
-      }
   }
-
-  int maxValueLen = maxEntry.getValue().toString().length();
-  ArrayList<String> results = new ArrayList<>();
-  for (Map.Entry<String, Integer> set : sjHash.entrySet()) {
-      String value = set.getValue().toString();
-      while (value.length() < maxValueLen) {
-          value = " " + value;
-      }
-      results.add(value + " of " + set.getKey());
-  }
-
-  Collections.sort(results);
-  Collections.reverse(results);
-  for (int i = 0; i < results.size() && i < 100; i++) {
-      System.out.println(results.get(i)); //end of copied code from CS1111 website
-  }
-}
-
-
 
 }
--- Sentence.java
+++ Sentence.java
@@ -41,8 +41,35 @@ public class Sentence{
   }
 
   public ArrayList<String> splitSentence() { // (used HW8 as inspo) this part splits the sentence of the tweet into seperate words
+
+    String[] stopwords = {
+      "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", 
+      "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", 
+      "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", 
+      "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", 
+      "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", 
+      "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", 
+      "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", 
+      "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", 
+      "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", 
+      "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", 
+      "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", 
+      "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", 
+      "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", 
+      "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", 
+      "you've", "your", "yours", "yourself", "yourselves", "ain't", "aren", "couldn", "didn", "doesn", "hadn", 
+      "hasn", "haven", "isn", "ma", "mightn", "mustn", "needn", "shan", "shouldn", "wasn", "weren", "won", 
+      "wouldn", "ain", "aren't", "couldn't", "didn't", "doesn't", "hadn't", "hasn't", "haven't", "isn't", 
+      "mightn't", "mustn't", "needn't", "shan't", "shouldn't", "wasn't", "weren't", "won't", "wouldn't", 
+      "daren", "daren't", "oughtn", "oughtn't","go","let"}; 
+
 	ArrayList<String> words = new ArrayList<>();
-	String[] splitWords = this.text.split(" ");
+
+	String[] splitWords = text.split(" ");
+  //for loop thru splitwords
+  //if splitwords[i] is in stopwords, do not add
+  //else, add it
+
 	words.addAll(Arrays.asList(splitWords));
 	return words;
 
@@ -51,9 +78,10 @@ public class Sentence{
 
 
 
+
     // for Twitter COVID dataset
     public static Sentence convertLine(String line){
-      System.out.println(line);
+      // System.out.println(line);
       String[] pieces = new String[8];
 
       String basket = "";
@@ -71,7 +99,7 @@ public class Sentence{
         }
       }
       pieces[ctr] = basket;
-      System.out.println(Arrays.toString(pieces));
+     // System.out.println(Arrays.toString(pieces));
       String date = pieces[2];
       String username = pieces[4];
       String tweet = pieces[7];
@@ -112,11 +140,10 @@ public class Sentence{
       else if (month.equals("12"))
         month = "December";
 
-      date = month + " " + datePieces[1] + " " +  datePieces[2];
+      date = month + " " + datePieces[1] + " 20" +  datePieces[2];
   
       String author = username;
       String text = tweet;
       return new Sentence(text, author, date);
     }
   }
-
